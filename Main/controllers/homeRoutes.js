9c3c16eef2b0f7a8fd07873f84624208547a951d
Hashimjasem <you@example.com>
2023-04-08 21:58:54 +0800

    added the models for DB

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -17,18 +17,9 @@ const withAuth = require('../utils/auth');
 
 
 router.get('/', async (req, res) => {
-  try {
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  res.render('landing', {
+    user: req.session
+  })
 });
 
 router.get('/project/:id', async (req, res) => {
